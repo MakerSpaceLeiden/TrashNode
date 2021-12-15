263f0a0da73c6cb4cd4782081752b80f1a8697aa
Duco Fijma <duco@fijma.net>
2021-12-15 17:11:23 +0100

    init; have ethernet and a blinking led

---
+++ README.md
@@ -0,0 +1,9 @@
+https://www.olimex.com/Products/IoT/ESP32/ESP32-POE/open-source-hardware
+
+
+- CH341 Serial
+-
+
+
+https://dronebotworkshop.com/esp32-intro/
+https://www.instructables.com/Getting-Started-With-ESP32-on-a-Mac-Blink-and-LED/ 
---
+++ trashnode.ino
@@ -0,0 +1,90 @@
+/*
+    This sketch shows the Ethernet event usage
+*/
+#define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
+#define ETH_PHY_POWER 12
+
+#include <ETH.h>
+
+static bool eth_connected = false;
+
+const int LEDPIN = 5;
+
+void WiFiEvent(WiFiEvent_t event)
+{
+  Serial.print("ETH EVENT"); Serial.println(event);
+  Serial.println(SYSTEM_EVENT_ETH_START);
+  switch (event) {
+    case ARDUINO_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+void testClient(const char * host, uint16_t port)
+{
+  Serial.print("\nconnecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
+  while (client.connected() && !client.available());
+  while (client.available()) {
+    Serial.write(client.read());
+  }
+
+  Serial.println("closing connection\n");
+  client.stop();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.onEvent(WiFiEvent);
+  ETH.begin(ETH_PHY_ADDR, 12 /* power */, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_LAN8720, ETH_CLOCK_GPIO17_OUT);
+  pinMode(LEDPIN, OUTPUT);
+}
+
+
+void loop()
+{
+  digitalWrite(LEDPIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  if (eth_connected) {
+    testClient("google.com", 80);
+  }
+  delay(5000);
+  digitalWrite(LEDPIN, LOW); 
+  delay(5000);
+}
